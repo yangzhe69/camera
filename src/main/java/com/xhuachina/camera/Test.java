9ae41c353e489590f6791e2f49c6c1058fdff69f
yangzhe <289264969@qq.com>
2020-12-09 12:42:01 +0800

    创建分之

--- src/main/java/com/xhuachina/camera/Test.java
+++ src/main/java/com/xhuachina/camera/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.printf("hahah");
         System.out.printf("创建分支");
+        System.out.printf("主干添加");
     }
 }
