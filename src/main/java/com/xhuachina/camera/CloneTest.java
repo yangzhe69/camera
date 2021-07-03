a2589e7c0d48462e7b55c49c4ee7da5354a3fe02
yangzhe <15707206830@163.com>
2021-07-03 11:02:53 +0800

    再次

--- src/main/java/com/xhuachina/camera/CloneTest.java
+++ src/main/java/com/xhuachina/camera/CloneTest.java
@@ -1,2 +1,11 @@
-package com.xhuachina.camera;public class CloneTest {
+package com.xhuachina.camera;
+
+public class CloneTest {
+
+    public static void main(String[] args) {
+//        Person p = new Person(23, "zhang");
+//        Person p1 = (Person) p.clone();
+//        String result = p.getName() == p1.getName() ? "clone 是浅拷贝的" : "clone 是深拷贝的";
+//        System.out.println(result);
+    }
 }
